e3de98fd0123d1a1f970f1f69d2e50b1605148f4
JuanCarlos1205 <jacho.juan.carlos@gmail.com>
2026-04-16 18:50:55 -0300

    Alteracao exemplo 02

--- exemplo02.py
+++ exemplo02.py
@@ -35,12 +35,13 @@ print('Insira os valores da venda')
 resposta = 'S'
 while resposta != 'N':
     v = float(input("Insira o valor da venda: "))
-    if v >= 1000:
+    if v > 1000:
         d = 0.1*v 
+        v -= d 
         print(f'O valor da venda e: {v}')
         print(f'O valor de desconto e: {d}')
     else: 
         print(f'O valor da venda e: {v}')
     resposta = input('Quer continuar? [S/N]: ').upper().strip()[0]
-
+print('Programa encerrado')
 
